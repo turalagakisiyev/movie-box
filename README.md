e9b6b92501808794ee47c3cc54dd3753d579b75c
turalagakisiyev <114071593+turalagakisiyev@users.noreply.github.com>
2022-12-13 22:02:30 +0400

    Add files via upload
    sjsjs

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# movie-box
+# Movie
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,38 @@
+import React  from 'react';
+
+import { useSelector } from 'react-redux';
+import MovieItem from '../MovieItem/MovieItem';
+import './Movies.css';
+
+const Movies = () => {
+
+    const { loading, movies, error } = useSelector(state => state.movies)
+
+
+
+    if(loading){
+    return <>loading.....</>
+    }
+
+    return (
+
+        <>
+
+            {
+                <ul className="movies">
+                    {movies?.length > 0 ? movies.map((movie) => (
+                        <li className="movies__item" key={movie.imdbID}>
+                            <MovieItem {...movie} />
+                        </li>
+                    )) : error ? <h1>error</h1> : <>Enter name of the movie</>}
+                </ul>
+
+            }
+
+        </>
+        
+    );
+
+}
+
+export default Movies;
